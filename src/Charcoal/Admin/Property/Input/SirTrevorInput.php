d7860b28b0f86512e0915de8bdc6788547d25f27
JoelAlphonso <admin@joeldesign.ca>
2016-07-05 11:42:20 -0400

    sir-trevor input options

--- src/Charcoal/Admin/Property/Input/SirTrevorInput.php
+++ src/Charcoal/Admin/Property/Input/SirTrevorInput.php
@@ -2,8 +2,7 @@
 
 namespace Charcoal\Admin\Property\Input;
 
-use \Charcoal\Admin\Property\AbstractSelectableInput;
-use Charcoal\Translation\TranslationString;
+use Charcoal\Admin\Property\AbstractPropertyInput;
 use Mmes\Support\Traits\ParsableValueTrait;
 
 /**
@@ -12,77 +11,26 @@ use Mmes\Support\Traits\ParsableValueTrait;
  * > The HTML _select_ (`<select>`) element represents a control that presents a menu of options.
  * — {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select}
  */
-class SirTrevorInput extends AbstractSelectableInput
+class SirTrevorInput extends AbstractPropertyInput
 {
     use ParsableValueTrait;
 
     protected $searchable;
     protected $options;
-    protected $dualSelectOptions;
+    protected $sirTrevorOptions;
 
     // FUNCTIONS
     // ==========================================================================
 
-    /**
-     * Retrieve the unselected options.
-     *
-     * @return Generator|array
-     */
-    public function unselectedChoices()
-    {
-        $choices = parent::choices();
-
-        /* [^1] */
-        foreach ($choices as $choice) {
-            if (!$choice['selected']) {
-                yield $choice;
-            }
-        }
-    }
-
-    /**
-     * Retrieve the selectable options.
-     *
-     * @return Generator|array
-     */
-    public function selectedChoices()
-    {
-        $val = $this->p()->val();
-
-        if ($val !== null) {
-
-            $val = $this->p()->parseVal($val);
-
-            if (!$this->p()->multiple()) {
-                $val = [$val];
-            }
-
-            $choices = iterator_to_array(parent::choices());
-
-            /* [^1] */
-            foreach ($val as $v) {
-                if (isset($choices[$v])) {
-                    yield $choices[$v];
-                }
-            }
-        }
-    }
-
     public function options()
     {
-        $opts = $this->dualSelectOptions;
+        $opts = $this->sirTrevorOptions;
 
         $optionName = array_keys($opts);
 
         foreach( $optionName as $optName ) {
-            // if($optName === 'sort' && $opts[$optName] === false) {
-            //     $this->options[$optName] = $this->defaultSort();
-            //     continue;
-            // }
             $this->options[$optName] = $opts[$optName];
         }
-
-        // error_log(var_export($this->options, true));
 
         if ($this->options) {
             return json_encode($this->options);
@@ -91,70 +39,15 @@ class SirTrevorInput extends AbstractSelectableInput
         }
     }
 
-    public function defaultSort()
-    {
-        return iterator_to_array(parent::choices());
-    }
-
     // GETTERS
     // ==========================================================================
 
     /**
      * @return mixed
      */
-    public function searchable()
+    public function sirTrevorOptions()
     {
-        $this->searchable = $this->dualSelectOptions['searchable'];
-
-        $placeholder = new TranslationString([
-            "en" => "Search...",
-            "fr" => "Recherche..."
-        ]);
-
-        $defaultOptions = [
-            "left"  => [
-                "placeholder" => $placeholder
-            ],
-            "right" => [
-                "placeholder" => $placeholder
-            ]
-        ];
-
-        if (is_bool($this->searchable) && $this->searchable) {
-
-            $this->searchable = $defaultOptions;
-
-        } else if (is_array($this->searchable)) {
-
-            if (isset($this->searchable['left']['placeholder'])) {
-                $this->searchable['left']['placeholder'] = $this->parseTranslatable($this->searchable['left']['placeholder']);
-            } elseif (isset($this->searchable['placeholder'])) {
-                $this->searchable['left']['placeholder'] = $this->parseTranslatable($this->searchable['placeholder']);
-            } else {
-                $this->searchable['left']['placeholder'] = $placeholder;
-            }
-
-            if (isset($this->searchable['right']['placeholder'])) {
-                $this->searchable['right']['placeholder'] = $this->parseTranslatable($this->searchable['right']['placeholder']);
-            } elseif (isset($this->searchable['placeholder'])) {
-                $this->searchable['right']['placeholder'] = $this->parseTranslatable($this->searchable['placeholder']);
-            } else {
-                $this->searchable['right']['placeholder'] = $placeholder;
-            }
-
-        } else {
-            $this->searchable = false;
-        }
-
-        return $this->searchable;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function dualSelectOptions()
-    {
-        return $this->dualSelectOptions;
+        return $this->sirTrevorOptions;
     }
 
     // SETTERS
@@ -162,12 +55,12 @@ class SirTrevorInput extends AbstractSelectableInput
 
 
     /**
-     * @param mixed $dualSelectOptions
-     * @return DualSelectInput
+     * @param mixed $sirTrevorOptions
+     * @return sirTrevorOptions
      */
-    public function setDualSelectOptions($dualSelectOptions)
+    public function setDualSelectOptions($sirTrevorOptions)
     {
-        $this->dualSelectOptions = $dualSelectOptions;
+        $this->sirTrevorOptions = $sirTrevorOptions;
         return $this;
     }
 
